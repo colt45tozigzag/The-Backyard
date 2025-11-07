2a8b72a3329ca15b381811a667136fca812426c5
colt45tozigzag <colton.ming2001@gmail.com>
2025-11-07 11:56:44 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+const toggle = document.getElementById("nav-toggle");
+const links = document.getElementById("nav-links");
+
+toggle.addEventListener("click", () => {
+  links.classList.toggle("show");
+});
